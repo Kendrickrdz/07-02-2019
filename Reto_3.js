ae297bf791232ed4051322e50180b912c4576a3c
Kendrickrdz <42614741+Kendrickrdz@users.noreply.github.com>
2019-02-07 09:24:21 -0600

    Create Reto_3.js

---
+++ Reto_3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	strokeWeight(30);
+	ellipse(200,200,250,250);
+	strokeWeight(30);
+	stroke(255,0,0);
+	line(120,120,280,280);
+	
+}
